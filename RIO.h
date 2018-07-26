fca5ec1fb954f8988df45598a1832fb670d076d8
yngzij <240356965@qq.com>
2018-07-26 19:05:31 +0800

    delect nocopy

--- RIO.h
+++ RIO.h
@@ -5,32 +5,26 @@
 #pragma once
 
 #include <zconf.h>
-#include <boost/noncopyable.hpp>
 #define RIO_BUFSIZE 8192
 
 typedef struct {
-    int rio_fd;                /* Descriptor for this internal buf */
-    ssize_t rio_cnt;               /* Unread bytes in internal buf */
-    char *rio_bufptr;          /* Next unread byte in internal buf */
-    char rio_buf[RIO_BUFSIZE]; /* Internal buffer */
+  int rio_fd;                /* Descriptor for this internal buf */
+  ssize_t rio_cnt;           /* Unread bytes in internal buf */
+  char *rio_bufptr;          /* Next unread byte in internal buf */
+  char rio_buf[RIO_BUFSIZE]; /* Internal buffer */
 } rio_t;
 
-
-class RIO :boost::noncopyable{
+class RIO {
 public:
-    RIO();
-    ~RIO();
-    void rio_readinitb(int fd);
-    ssize_t rio_readlineb(char *usrbuf,size_t maxlen);
-    ssize_t rio_readnb(char *usrbuf,size_t n);
-    ssize_t rio_readn(char *usrbuf,size_t n);
-    ssize_t rio_writen(char *usrbuf,size_t n);
-    ssize_t rio_read(char *usrbuf,size_t n);
-
+  RIO();
+  ~RIO();
+  void rio_readinitb(int fd);
+  ssize_t rio_readlineb(char *usrbuf, size_t maxlen);
+  ssize_t rio_readnb(char *usrbuf, size_t n);
+  ssize_t rio_readn(char *usrbuf, size_t n);
+  ssize_t rio_writen(char *usrbuf, size_t n);
+  ssize_t rio_read(char *usrbuf, size_t n);
 
 private:
-    rio_t *rp;
+  rio_t *rp;
 };
-
-
-
